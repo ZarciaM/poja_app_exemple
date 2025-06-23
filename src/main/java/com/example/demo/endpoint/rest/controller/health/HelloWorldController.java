55a759767ca9e1285ffdd340fd969230d0dd2610
ZarciaM <154671918+ZarciaM@users.noreply.github.com>
2025-06-23 14:01:56 +0300

    Update HelloWorldController.java

--- src/main/java/com/example/demo/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloWorldController.java
@@ -1,9 +1,11 @@
 package com.example.demo.endpoint.rest.controller.health;
 
-
-
-import com.example.demo.service.HelloWorldService;
+import com.exemple.endpoint.mail.Email;
+import com.exemple.endpoint.mail.Mailer;
+import jakarta.mail.internet.InternetAddress;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
-  private final HelloWorldService service;
+  private final Mailer mailer;
 
   @GetMapping("/hello")
-  public String helloWorld(@RequestParam String name) {
-    return service.uploadHelloWorldMessage(name);
+  @SneakyThrows
+  public String helloWorld(@RequestParam String to) {
+    var email =
+        new Email(new InternetAddress(to), List.of(), List.of(), "Hello world", "... world!", List.of());
+
+    mailer.accept(email);
+    return "... world!";
   }
 }
-
